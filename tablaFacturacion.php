54fa58f95702b623306d0d6647b329faaf566394
sofiasartori <sofiainessartori@hotmail.com>
2018-04-09 22:02:05 -0300

    Delete tablaFacturacion.php

--- tablaFacturacion.php
+++
@@ -1,6 +0,0 @@
- <table border=1><th> patente </th><th> Importe </th><tr> <td> abc123</td> <td>   $300
-</td> </tr><tr> <td> rte456</td> <td>   $9825
-</td> </tr><tr> <td> ivv033</td> <td>   $52230
-</td> </tr><tr> <td> ivv022</td> <td>   $120
-</td> </tr><tr> <td> gte211</td> <td>   $585
-</td> </tr> </table>
